fd6f54f4233305e8e9b3bbd3d5326980da4d3926
arufiand <alfiandannyarmanta@gmail.com>
2024-07-03 23:12:38 +0700

    setting up cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require("cors");
 
 // Constants
 const PORT = 8080;
@@ -9,6 +10,19 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+//cors
+let corsOptions = {
+    origin: `http://${HOST}:${PORT}`
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('Hello Worldss');
 });
