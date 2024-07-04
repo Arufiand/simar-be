5736145be856af7a70b0397e9fca24d5afd79152
arufiand <alfiandannyarmanta@gmail.com>
2024-07-04 21:22:08 +0700

    Development Sequelize

--- server.js
+++ server.js
@@ -26,14 +26,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-db.sequelize.sync()
-    .then(() => {
-        console.log("Synced db.");
-    })
-    .catch((err) => {
-        console.log("Failed to sync db: " + err.message);
-    });
-
+// db.sequelize.sync()
+//     .then(() => {
+//         console.log("Synced db.");
+//     })
+//     .catch((err) => {
+//         console.log("Failed to sync db: " + err.message);
+//     });
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
 app.get('/', (req, res) => {
     res.send('Hello Worldss');
 });
