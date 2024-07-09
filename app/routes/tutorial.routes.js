42515b66bfa5327044022d9e4e37c4340fa667f3
Alfian Danny Armanta <alfiandannyarmanta@gmail.com>
2024-07-09 11:15:10 +0700

    feat(API): updating tutorial with token

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -1,28 +1,36 @@
-module.exports = app => {
-    const tutorials = require("../controller/tutorial.controller.js");
+const { authJwt } = require("../middleware");
+const controller = require("../controller/tutorial.controller.js");
 
+module.exports = function(app){
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
     var router = require("express").Router();
 
     // Create a new Tutorial
-    router.post("/", tutorials.create);
+    router.post("/", controller.create);
 
     // Retrieve all Tutorials
-    router.get("/", tutorials.findAll);
+    router.get("/", controller.findAll);
 
     // Retrieve all published Tutorials
-    router.get("/published", tutorials.findAllPublished);
+    router.get("/published", controller.findAllPublished);
 
     // Retrieve a single Tutorial with id
-    router.get("/:id", tutorials.findOne);
+    router.get("/:id", controller.findOne);
 
     // Update a Tutorial with id
-    router.put("/:id", tutorials.update);
+    router.put("/:id", [authJwt.verifyToken, authJwt.isModerator], controller.update);
 
     // Delete a Tutorial with id
-    router.delete("/:id", tutorials.delete);
+    router.delete("/:id",[authJwt.verifyToken, authJwt.isModerator], controller.delete);
 
     // Delete all Tutorials
-    router.delete("/", tutorials.deleteAll);
+    router.delete("/",[authJwt.verifyToken, authJwt.isModerator], controller.deleteAll);
 
     app.use('/api/tutorials', router);
 };
