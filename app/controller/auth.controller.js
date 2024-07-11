4a4b1c996bbcd68af8d43e424c02b0a71dd5339d
Alfian Danny Armanta <alfiandannyarmanta@gmail.com>
2024-07-11 15:14:12 +0700

    feat(API): #1 adding Commit on signup

--- app/controller/auth.controller.js
+++ app/controller/auth.controller.js
@@ -10,48 +10,55 @@ const Op = db.Sequelize.Op;
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
-exports.signup = (req, res) => {
-    // Save User to Database
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8)
-    })
-        .then(user => {
-            if (req.body.roles) {
-                Role.findAll({
-                    where: {
-                        name: {
-                            [Op.or]: req.body.roles
-                        }
-                    }
-                }).then(roles => {
-                    user.setRoles(roles).then(() => {
-                        Division.findOne({ where: { id: req.body.division } }).then(division => {
-                            UserDetail.create({
-                                userId : user.id,
-                                fullName : req.body.fullName,
-                                birthDate : req.body.birthDate,
-                                gender : req.body.gender,
-                                divisionID : division.id
-                            })
-                        });
-                        res.send({ message: "User was registered successfully!" });
-                    });
+exports.signup = async (req, res) => {
+    const t = await db.sequelize.transaction();
 
-                });
+    try {
+        // Check for roles and division first
+        const roles = req.body.roles ? await Role.findAll({
+            where: {
+                name: {
+                    [Op.or]: req.body.roles
+                }
+            },
+            transaction: t
+        }) : null;
 
+        const division = await Division.findOne({ where: { id: req.body.division }, transaction: t });
+        console.log(`division ${division}`);
+        console.log(`division body ${req.body.division}`);
+        if (!division) {
+            throw new Error('Division not found!');
+        }
 
-            } else {
-                // user role = 1
-                user.setRoles([1]).then(() => {
-                    res.send({ message: "User was registered successfully" });
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
-        });
+        // Save User to Database
+        const user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 8)
+        }, { transaction: t });
+
+        if (roles) {
+            await user.setRoles(roles, { transaction: t });
+        } else {
+            await user.setRoles([1], { transaction: t });
+        }
+
+        await UserDetail.create({
+            userId: user.id,
+            fullName: req.body.fullName,
+            birthDate: req.body.birthDate,
+            gender: req.body.gender,
+            divisionID: division.id
+        }, { transaction: t });
+
+        await t.commit();
+        res.send({ message: "User was registered successfully!" });
+    } catch (error) {
+        await t.rollback();
+        console.error('Error occurred:', error);
+        res.status(500).send({ message: error.message });
+    }
 };
 
 exports.signin = (req, res) => {
